f96ca3ab1be2884ed16eb07b0d769efee4fdc91f
Bambino99 <giovannisciotto08@gmail.com>
2026-03-11 18:23:57 +0100

    v0.5 Sistemazione musica e gameOver FIX font troppo alta quando perdi

--- src/main/java/torricelli/tetris_project/FXMLController.java
+++ src/main/java/torricelli/tetris_project/FXMLController.java
@@ -4,6 +4,11 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.ResourceBundle;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
@@ -22,19 +27,16 @@ public class FXMLController implements Initializable {
 	private Pane[][] gridNodes; // Per gestire i riferimenti ai quadratini
 	private Blocco blocco;
 	private Timeline timeline;
-
-	private long punteggio = 0;
+	private Clip musica;
 
 	private int level = 0;
 	private int linee = 0;
 	private int lineeTemp = 0;
 	private int speed = 1000;
-	private int changeSpeed = 200;
-	private long highscoreint;
+	private int prossimoTipo = -1;
 
-	// --- VARIABILI AGGIUNTE ---
-	private int prossimoTipo = -1; // Memorizza l'indice del blocco che arriverà dopo
-	// ---------------------------
+	private long punteggio = 0;
+	private long highscoreint;
 
 	private boolean gameOver = false;
 	private boolean isAnimating = false; // Per bloccare il gioco durante i flash
@@ -50,6 +52,9 @@ public class FXMLController implements Initializable {
 
 	@FXML
 	private GridPane nextBlockGrid;
+
+	@FXML
+	private Label titolo;
 
 	@FXML
 	private Label highScore;
@@ -69,31 +74,43 @@ public class FXMLController implements Initializable {
 		KeyCode code = event.getCode();
 
 		if (code == KeyCode.UP && !gameOver) {
+
 			blocco.rotate();
 		}
 
 		if (code == KeyCode.DOWN && !gameOver) {
+
 			punteggio += blocco.moveDown();
-			if(checkhighscore()){
-				highscoreint=punteggio;
-			}
+
+			if (checkhighscore())
+				highscoreint = punteggio;
+
 			updateLabels();
 		}
 
 		if (code == KeyCode.LEFT && !gameOver) {
+
 			blocco.moveLeft();
 		}
 
 		if (code == KeyCode.RIGHT && !gameOver) {
+
 			blocco.moveRight();
 		}
 
 		if (code == KeyCode.SPACE && !gameOver) {
+
 			punteggio += blocco.confirm();
-			if(checkhighscore()){
-				highscoreint=punteggio;
-			}
+
+			if (checkhighscore())
+				highscoreint = punteggio;
+
 			updateLabels();
+		}
+
+		if (code == KeyCode.SPACE && gameOver) {
+
+			gameStart();
 		}
 	}
 
@@ -104,21 +121,39 @@ public class FXMLController implements Initializable {
 		mainGrid.setOnKeyPressed(this::onKeyPressed);
 		mainGrid.requestFocus();
 
-		gridNodes = new Pane[mainGrid.getRowCount()][mainGrid.getColumnCount()];
-		occupied = new boolean[mainGrid.getRowCount()][mainGrid.getColumnCount()];
+		try {
 
-		for (boolean[] booleans : occupied)
-			Arrays.fill(booleans, false);
+			// Prendo la musica
+			AudioInputStream audio = AudioSystem
+					.getAudioInputStream(getClass().getResource("/audio/Tetris-Theme-Piano.wav"));
 
-		// --- LOGICA AGGIUNTA ---
-		prossimoTipo = rand.nextInt(7); // Determina il primo "prossimo blocco"
-		updateNextBlockGrid();          // Lo disegna nella griglia piccola
-		// -----------------------
+			musica = AudioSystem.getClip();
+			musica.open(audio);
 
-		game();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		gameStart();
 	}
 
-	public void game() {
+	public void gameStart() {
+
+		mainGrid.getChildren().clear();
+
+		level = 0;
+		linee = 0;
+		lineeTemp = 0;
+		speed = 1000;
+		prossimoTipo = -1;
+
+		punteggio = 0;
+
+		gameOver = false;
+		isAnimating = false; // Per bloccare il gioco durante i flash
+		titolo.setText("TETRISFX");
+		titolo.setStyle("-fx-font-size: 48px");
 
 		punteggio = 0;
 		linee = 0;
@@ -126,8 +161,19 @@ public class FXMLController implements Initializable {
 		speed = 1000;
 		highscoreint = TextUtility.HighScoreManager.loadHighScore();
 
+		gridNodes = new Pane[mainGrid.getRowCount()][mainGrid.getColumnCount()];
+		occupied = new boolean[mainGrid.getRowCount()][mainGrid.getColumnCount()];
+
+		for (boolean[] booleans : occupied)
+			Arrays.fill(booleans, false);
+
+		prossimoTipo = rand.nextInt(7); // Determina il primo "prossimo blocco"
+		updateNextBlockGrid(); // Lo disegna nella griglia piccola
+
 		updateLabels();
 		spawnBlock();
+		
+		startMusic();
 
 		timeline = new Timeline(new KeyFrame(Duration.millis(speed), e -> gameTick()));
 		timeline.setCycleCount(Timeline.INDEFINITE);
@@ -135,22 +181,35 @@ public class FXMLController implements Initializable {
 	}
 
 	private void gameTick() {
+
 		if (gameOver) {
-			if(punteggio == highscoreint){
-				System.out.println(GREEN+"NEW HIGH SCORE!"+RESET);
+
+			stopMusic();
+			
+			if (punteggio == highscoreint) {
+
+				System.out.println(GREEN + "NEW HIGH SCORE!" + RESET);
 				TextUtility.HighScoreManager.saveHighScore(highscoreint);
 			}
-			System.out.println(RED + "[FINE PROGRAMMA]" + YELLOW + " Utente ha perso." + GREEN + " Score: "
-					+ punteggio + " High score: " + highscoreint + RESET);
+
+			System.out.println(RED + "[FINE PROGRAMMA]" + YELLOW + " Utente ha perso." + GREEN + " Score: " + punteggio
+					+ " High score: " + highscoreint + RESET);
+
+			titolo.setText("HAI PERSO!");
+			titolo.setStyle("-fx-font-size: 39px");
 			timeline.stop();
-			System.exit(0);
+
 		} else {
+
 			if (isAnimating)
 				return;
 
 			if (blocco.getIsFalling()) {
+
 				blocco.fall();
+
 			} else {
+
 				registerBlockInGrid();
 				checkAndClearLines();
 			}
@@ -158,15 +217,20 @@ public class FXMLController implements Initializable {
 	}
 
 	private void registerBlockInGrid() {
+
 		int rot = blocco.getRotazione();
 		Pane[] panes = blocco.getPanes();
 		int[][] px = blocco.getPosX();
 		int[][] py = blocco.getPosY();
 
+		// Registro il blocco all'interno della griglia
 		for (int i = 0; i < panes.length; i++) {
+
 			int r = py[rot][i];
 			int c = px[rot][i];
+
 			if (r >= 0 && r < mainGrid.getRowCount()) {
+
 				gridNodes[r][c] = panes[i];
 				occupied[r][c] = true;
 			}
@@ -174,51 +238,80 @@ public class FXMLController implements Initializable {
 	}
 
 	private void checkAndClearLines() {
+
+		// Controllo se le linee sono piene e in caso le rimuovo
 		for (int r = mainGrid.getRowCount() - 1; r >= 0; r--) {
+
 			boolean full = true;
+
 			for (int c = 0; c < mainGrid.getColumnCount(); c++) {
+
 				if (!occupied[r][c]) {
+
 					full = false;
 					break;
 				}
 			}
 
 			if (full) {
+
 				animateAndRemoveLine(r);
-				lineeTemp++;
+				lineeTemp++; // Aumento un valore temporaneo
 				return;
 			}
 		}
 
+		// Aumento il punteggio
 		switch (lineeTemp) {
-			case 1: punteggio += (long)40 * (level + 1); break;
-			case 2: punteggio += (long)100 * (level + 1); break;
-			case 3: punteggio += (long)300 * (level + 1); break;
-			case 4: punteggio += (long)1200 * (level + 1); break;
+
+		case 1:
+
+			punteggio += (long) 40 * (level + 1);
+			break;
+
+		case 2:
+
+			punteggio += (long) 100 * (level + 1);
+			break;
+
+		case 3:
+
+			punteggio += (long) 300 * (level + 1);
+			break;
+
+		case 4:
+
+			punteggio += (long) 1200 * (level + 1);
+			break;
 		}
 
+		// Aumento il numero di linee effettive
 		linee += lineeTemp;
 		lineeTemp = 0;
 
-		if (linee / 10 > level) {
+		// Cambio il livello se ho tolto 10 linee
+		if (linee / 10 > level)
 			changeLevel();
-		}
-		if(checkhighscore()){
+
+		// Cambio high score se è maggiore
+		if (checkhighscore())
 			highscoreint = punteggio;
-		}
+
 		updateLabels();
 		spawnBlock();
 	}
-	private boolean checkhighscore(){
-		return (punteggio>highscoreint);
+
+	private boolean checkhighscore() {
+
+		return (punteggio > highscoreint);
 	}
 
 	private void animateAndRemoveLine(int row) {
+
 		isAnimating = true;
 		final int r = row;
 
-		Timeline flash = new Timeline(
-				new KeyFrame(Duration.ZERO, e -> setRowColor(r, "white")),
+		Timeline flash = new Timeline(new KeyFrame(Duration.ZERO, e -> setRowColor(r, "white")),
 				new KeyFrame(Duration.millis(150), e -> setRowColor(r, "darkviolet")),
 				new KeyFrame(Duration.millis(300), e -> setRowColor(r, "white")),
 				new KeyFrame(Duration.millis(450), e -> {
@@ -232,15 +325,21 @@ public class FXMLController implements Initializable {
 	}
 
 	private void setRowColor(int row, String color) {
+
 		for (int c = 0; c < mainGrid.getColumnCount(); c++) {
+
 			if (gridNodes[row][c] != null) {
+
 				gridNodes[row][c].setStyle("-fx-background-color: " + color + "; -fx-border-color: black;");
 			}
 		}
 	}
 
 	private void removeLine(int row) {
+
 		for (int c = 0; c < mainGrid.getColumnCount(); c++) {
+
+			// Rimuovo i pane alla linea corrente
 			mainGrid.getChildren().remove(gridNodes[row][c]);
 			gridNodes[row][c] = null;
 			occupied[row][c] = false;
@@ -248,9 +347,16 @@ public class FXMLController implements Initializable {
 	}
 
 	private void shiftDown(int deletedRow) {
+
+		// r = riga
+		// c = colonna
 		for (int r = deletedRow - 1; r >= 0; r--) {
+
 			for (int c = 0; c < mainGrid.getColumnCount(); c++) {
+
 				if (gridNodes[r][c] != null) {
+
+					// Sposto tutti i nodi in basso di uno cambiando i riferimenti e gli indici
 					gridNodes[r + 1][c] = gridNodes[r][c];
 					occupied[r + 1][c] = true;
 					GridPane.setRowIndex(gridNodes[r + 1][c], r + 1);
@@ -262,41 +368,75 @@ public class FXMLController implements Initializable {
 	}
 
 	public void spawnBlock() {
-		// --- MODIFICATA PER USARE PROSSIMOTIPO ---
+
 		int tipoAttuale = prossimoTipo;
 
 		switch (tipoAttuale) {
-			case 0: blocco = new Punta(mainGrid, occupied); break;
-			case 1: blocco = new Lungo(mainGrid, occupied); break;
-			case 2: blocco = new Quadrato(mainGrid, occupied); break;
-			case 3: blocco = new Lsinistra(mainGrid, occupied); break;
-			case 4: blocco = new Ldestra(mainGrid, occupied); break;
-			case 5: blocco = new Zsinistra(mainGrid, occupied); break;
-			case 6: blocco = new Zdestra(mainGrid, occupied); break;
+
+		case 0:
+
+			blocco = new Punta(mainGrid, occupied);
+			break;
+
+		case 1:
+
+			blocco = new Lungo(mainGrid, occupied);
+			break;
+
+		case 2:
+
+			blocco = new Quadrato(mainGrid, occupied);
+			break;
+
+		case 3:
+
+			blocco = new Lsinistra(mainGrid, occupied);
+			break;
+
+		case 4:
+
+			blocco = new Ldestra(mainGrid, occupied);
+			break;
+
+		case 5:
+
+			blocco = new Zsinistra(mainGrid, occupied);
+			break;
+
+		case 6:
+
+			blocco = new Zdestra(mainGrid, occupied);
+			break;
 		}
 
 		// Genera il prossimo e aggiorna la preview
 		prossimoTipo = rand.nextInt(7);
 		updateNextBlockGrid();
-		// ------------------------------------------
 
-		System.out.println(YELLOW + "[SPAWN]" + GREEN + " Spawn blocco tipo: " + blocco.getClass().getSimpleName() + RESET);
+		System.out.println(
+				YELLOW + "[SPAWN]" + GREEN + " Spawn blocco tipo: " + blocco.getClass().getSimpleName() + RESET);
 		blocco.draw();
 		gameOver = blocco.checkSpawn();
 	}
 
 	public void updateLabels() {
+
+		// Aggiorno tutti le scritte di cui ho bisogno
 		punteggioScore.setText(" Score: " + punteggio);
 		punteggioLines.setText(" Lines: " + linee);
 		punteggioSpeed.setText(" Speed: " + speed);
-		highScore.setText(" HIGH: "+ highscoreint);
+		highScore.setText(" HIGH: " + highscoreint);
 	}
 
 	public void changeLevel() {
-		level++;
-		speed -= changeSpeed;
-		if (speed < 100) speed = 100;
 
+		level++; // Aumento il livello
+
+		// La velocità diminuisce del venti per cento
+		if (speed >= 100)
+			speed -= (int) (speed * 20 / 100);
+
+		// Aggiorno la velocità a livello effettivo
 		timeline.stop();
 		timeline = new Timeline(new KeyFrame(Duration.millis(speed), e -> gameTick()));
 		timeline.setCycleCount(Timeline.INDEFINITE);
@@ -304,11 +444,8 @@ public class FXMLController implements Initializable {
 		updateLabels();
 	}
 
-	// ==========================================================
-	// METODI AGGIUNTI PER LA GESTIONE DELLA NEXT BLOCK GRID
-	// ==========================================================
-
 	private void updateNextBlockGrid() {
+
 		nextBlockGrid.getChildren().clear();
 
 		int[][] posXnext = {};
@@ -317,47 +454,84 @@ public class FXMLController implements Initializable {
 
 		// Definiamo le coordinate e il colore esatto per ogni pezzo
 		switch (prossimoTipo) {
-			case 0: // Punta
-				posXnext = new int[][]{{1, 0, 1, 2}}; posYnext = new int[][]{{0, 1, 1, 1}};
-				colorePezzo = javafx.scene.paint.Color.VIOLET;
-				break;
-			case 1: // Lungo
-				posXnext = new int[][]{{1, 1, 1, 1}}; posYnext = new int[][]{{0, 1, 2, 3}};
-				colorePezzo = javafx.scene.paint.Color.CYAN;
-				break;
-			case 2: // Quadrato
-				posXnext = new int[][]{{1, 2, 1, 2}}; posYnext = new int[][]{{1, 1, 2, 2}};
-				colorePezzo = javafx.scene.paint.Color.YELLOW;
-				break;
-			case 3: // L-Sinistra
-				posXnext = new int[][]{{1, 2, 1, 1}}; posYnext = new int[][]{{0, 0, 1, 2}};
-				colorePezzo = javafx.scene.paint.Color.ORANGE;
-				break;
-			case 4: // L-Destra
-				posXnext = new int[][]{{1, 2, 2, 2}}; posYnext = new int[][]{{0, 0, 1, 2}};
-				colorePezzo = javafx.scene.paint.Color.BLUE;
-				break;
-			case 5: // Z-Sinistra
-				posXnext = new int[][]{{1, 1, 2, 2}}; posYnext = new int[][]{{0, 1, 1, 2}};
-				colorePezzo = javafx.scene.paint.Color.RED;
-				break;
-			case 6: // Z-Destra
-				posXnext = new int[][]{{2, 2, 1, 1}}; posYnext = new int[][]{{0, 1, 1, 2}};
-				colorePezzo = javafx.scene.paint.Color.GREEN;
-				break;
+
+		case 0: // Punta
+
+			posXnext = new int[][] { { 1, 0, 1, 2 } };
+			posYnext = new int[][] { { 0, 1, 1, 1 } };
+			colorePezzo = javafx.scene.paint.Color.VIOLET;
+			break;
+
+		case 1: // Lungo
+
+			posXnext = new int[][] { { 1, 1, 1, 1 } };
+			posYnext = new int[][] { { 0, 1, 2, 3 } };
+			colorePezzo = javafx.scene.paint.Color.CYAN;
+			break;
+
+		case 2: // Quadrato
+
+			posXnext = new int[][] { { 1, 2, 1, 2 } };
+			posYnext = new int[][] { { 1, 1, 2, 2 } };
+			colorePezzo = javafx.scene.paint.Color.YELLOW;
+			break;
+
+		case 3: // L-Sinistra
+
+			posXnext = new int[][] { { 1, 2, 2, 2 } };
+			posYnext = new int[][] { { 0, 0, 1, 2 } };
+			colorePezzo = javafx.scene.paint.Color.ORANGE;
+			break;
+
+		case 4: // L-Destra
+
+			posXnext = new int[][] { { 1, 2, 1, 1 } };
+			posYnext = new int[][] { { 0, 0, 1, 2 } };
+			colorePezzo = javafx.scene.paint.Color.BLUE;
+			break;
+
+		case 5: // Z-Sinistra
+
+			posXnext = new int[][] { { 1, 1, 2, 2 } };
+			posYnext = new int[][] { { 0, 1, 1, 2 } };
+			colorePezzo = javafx.scene.paint.Color.RED;
+			break;
+
+		case 6: // Z-Destra
+
+			posXnext = new int[][] { { 2, 2, 1, 1 } };
+			posYnext = new int[][] { { 0, 1, 1, 2 } };
+			colorePezzo = javafx.scene.paint.Color.GREEN;
+			break;
 		}
 
 		// Creiamo il background usando lo stesso metodo della classe Punta
 		javafx.scene.layout.Background bg = new javafx.scene.layout.Background(
-				new javafx.scene.layout.BackgroundFill(colorePezzo, null, null)
-		);
+				new javafx.scene.layout.BackgroundFill(colorePezzo, null, null));
 
 		for (int i = 0; i < 4; i++) {
+
 			Pane p = new Pane();
 			p.setBackground(bg); // Applichiamo l'oggetto Background reale
-
-			// Se vuoi togliere i bordi bianchi assicurati che hgap e vgap della grid siano 0
 			nextBlockGrid.add(p, posXnext[0][i], posYnext[0][i]);
 		}
+	}
+
+	private void startMusic() {
+
+		// Faccio partire la musica se esiste
+		if (musica != null) {
+			
+			musica.setFramePosition(0);
+			musica.loop(Clip.LOOP_CONTINUOUSLY); // loop infinito
+			musica.start();
+		}
+	}
+	
+	private void stopMusic() {
+	 
+		// Fermo la musica
+		if (musica != null && musica.isRunning())
+	        musica.stop();
 	}
 }
